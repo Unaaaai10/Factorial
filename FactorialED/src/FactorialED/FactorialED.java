ecada3e2da23db10e074c39c09f001ad385d6a52
Unai <ulopez-cozarar20dw@ikzubirimanteo.com>
2021-12-13 13:12:40 +0100

    Funcion factorial

---
+++ FactorialED/src/FactorialED/FactorialED.java
@@ -0,0 +1,20 @@
+package FactorialED;
+
+public class FactorialED {
+
+	public static void factorial(int num) {
+		int count = 1;
+		int fact = 1;
+		while (count <= num) {
+			fact = fact * count;
+			count++;
+		}
+		System.out.println("Factorial: " + fact);
+	}
+
+	public static void main(String[] args) {
+		factorial(8);
+		factorial(12);
+		factorial(5);
+	}
+}
